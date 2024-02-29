162876f6ad59df1fc6025e94cf707b5667dc2a7d
Italo <italomlima13@gmail.com>
2024-02-29 17:31:42 -0300

    ultima verificação antes de mandar, fiz pouco mas fiz com amor. Um otimo projeto que mudou minha for ma de ver o mundo da programação.

--- javascript/main.js
+++ javascript/main.js
@@ -3,19 +3,8 @@ $('.carousel').slick({
     dots: true,
     infinite: true,
     speed: 300,
+    autoplay: true,
     slidestoshow: 3,
     adaptiveHeight: true
+    
 });
-// BUTAO
-function toggleOpcoes() {
-    const opcoes = document.querySelector('.opcoes');
-    if (opcoes.style.display === 'none') {
-      opcoes.style.display = 'block';
-    } else {
-      opcoes.style.display = 'none';
-    }
-  }
-
-  function escolha(opcao) {
-    alert('Opção selecionada: ' + opcao);
-  }
